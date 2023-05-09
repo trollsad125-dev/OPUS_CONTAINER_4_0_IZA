531f352eaedf5d653f88bf39ca220116930ae683
trollsad125-dev <77106876+trollsad125-dev@users.noreply.github.com>
2023-05-09 12:07:52 +0700

    Add Rule Practice 1

--- PRACTICE_001/jsp_n_js/esm/clv/clvtraining/errmsgmgmt/script/CLV_TRAINING_001.js
+++ PRACTICE_001/jsp_n_js/esm/clv/clvtraining/errmsgmgmt/script/CLV_TRAINING_001.js
@@ -10,25 +10,7 @@
 * 2023.04.19 
 * 1.0 Creation
 =========================================================*/
-/****************************************************************************************
-  이벤트 구분 코드: [초기화]INIT=0; [입력]ADD=1; [조회]SEARCH=2; [리스트조회]SEARCHLIST=3;
-					[수정]MODIFY=4; [삭제]REMOVE=5; [리스트삭제]REMOVELIST=6 [다중처리]MULTI=7
-					기타 여분의 문자상수  COMMAND01=11; ~ COMMAND20=30;
- ***************************************************************************************/
-
-/*------------------다음 코드는 JSDoc을 잘 만들기 위해서 추가된 코드임 ------------------*/
-   /**
-     * @fileoverview 업무에서 공통으로 사용하는 자바스크립트파일로 달력 관련 함수가 정의되어 있다.
-     * @author 한진해운
-     */
-
-    /**
-     * @extends 
-     * @class CLV_TRAINING_001 : CLV_TRAINING_001 생성을 위한 화면에서 사용하는 업무 스크립트를 정의한다.
-     */
-
     
-   	/* 개발자 작업	*/
 	var tabObjects=new Array();
 	var tabCnt=0 ;
 	var beforetab=1;
@@ -42,27 +24,20 @@
         sheetObjects[sheetCnt++]=sheet_obj;
      }
     function resizeSheet(){
-      	         ComResizeSheet(sheetObjects[0]);
-      }
-     /**
-      * body.onload 이벤트에서 호출되는 함수로 페이지 로드완료 후 선처리해야할 기능을 추가한다. <br>
-      * HTML컨트롤의 각종 이벤트와 IBSheet, IBTab 등을 초기화 한다. <br>
-      **/
+      ComResizeSheet(sheetObjects[0]);
+     }
+
      function loadPage() {
  		for(i=0;i<sheetObjects.length;i++){
-			//khlee-시작 환경 설정 함수 이름 변경
 			ComConfigSheet (sheetObjects[i]);
 			initSheet(sheetObjects[i],i+1);
-			//khlee-마지막 환경 설정 함수 추가
 			ComEndConfigSheet(sheetObjects[i]);
 			doActionIBSheet(sheetObjects[i],document.form,IBSEARCH)
 		}
  	}
 
     function processButtonClick(){
-        /***** 탭당 시트가 2개 이상인 경우엔 추가 시트변수 지정하여 사용한 *****/
         var sheetObject1=sheetObjects[0];
-        /*******************************************************/
         var formObject=document.form;
    	try {
    		var srcName=ComGetEvent("name");
@@ -121,63 +96,34 @@
         }
     }
 
-    function validateForm(sheetObj,formObj,sAction){
-        with(formObj){
-//            if (!isNumber(formObj.iPage)) {
-//                return false;
-//            }
-        }
-        return true;
-    }
     function doActionIBSheet(sheetObj,formObj,sAction) {
         switch(sAction) {
-		case IBSEARCH:      //조회
-			if(!validateForm(sheetObj,formObj,sAction)) return
-            //조회처리
+		case IBSEARCH:      //Search
 			ComOpenWait(true);
 			formObj.f_cmd.value=SEARCH;
 			sheetObj.DoSearch("CLV_TRAINING_001GS.do", FormQueryString(formObj) );
 			break;
-		case IBSAVE:        //저장
-			if(!validateForm(sheetObj,formObj,sAction))return;
-            //저장처리
+		case IBSAVE:        //Save
         	formObj.f_cmd.value=MULTI;
             sheetObj.DoSave("CLV_TRAINING_001GS.do", FormQueryString(formObj));
 			break;
-		case IBINSERT:      // 입력
+		case IBINSERT:      // Insert
 			rowcount=sheetObj.RowCount();
 			row=sheetObj.DataInsert(-1);
 			break;
-		case IBDOWNEXCEL:	//엑셀다운로드
+		case IBDOWNEXCEL:	//Download Excel
 			if(sheetObj.RowCount() < 1){
 				ComShowCodeMessage("Testing");
 			}else{
 				sheetObj.Down2Excel({DownCols: makeHiddenSkipCol(sheetObj), SheetDesign:1, Merge:1});
 			}
 			break;
-    }
+        }
     }
     function sheet1_OnSearchEnd(sheetObj, Code, Msg, StCode, StMsg) { 
      	ComOpenWait(false);
      }
-    function sheet1_OnPopupClick(sheetObj,Row,Col){
-        switch (sheetObj.ColSaveName(Col)) {
-       	case "dev_dt" :
-			var cal=new ComCalendarGrid();
-			cal.select(sheetObj, Row, Col, 'yyyyMMdd');
-            break;
-        case "roles" :
-			if(sheetObj.GetCellValue(Row,"ibflag")=="I" || sheetObj.GetCellValue(Row,"ibflag")=="U"){
-            	 ComShowCodeMessage('COM12151','Program');
-			}else if(sheetObj.GetCellValue(Row,"ibflag")=="R" && sheetObj.GetCellValue(Row,"pgm_mnu_div_cd")=="01"){
-				 ComShowCodeMessage('COM12190');
-             }else{
-            	 alert('wait!!!!!!!!')
-    		       //noRtnPopup('progRoleMapping.do?pgm_no='+sheetObj.CellValue(Row,"pgm_no")+"&pgm_nm="+escape(sheetObj.CellValue(Row,"pgm_nm")),'width=700,height=474,left=200,top=100,menubar=0,status=0,scrollbars=0,resizable=1');
-             }
-             break;
-        }
-	}
+    
      function sheet1_OnChange(sheetObj,Row,Col){
     	 if(Col == 2){
 			var code=sheetObj.GetCellValue(Row, Col);
@@ -185,7 +131,6 @@
 			var orlcode=sheetObj.GetCellValue(int, Col);
 			 null 인 경우와 자기 자신은 비교할 필요가 없음 
 				if(code != '' && int != Row && code == orlcode){
-    				 //ComShowMessage("동일한 Message Code가 존재합니다.");
     				 ComShowCodeMessage('COM131302',code);
     				 sheetObj.SetCellValue(Row, Col,"");
     				 return;
@@ -194,4 +139,3 @@
     	 }
      }
      
-	/* 개발자 작업  끝 */
